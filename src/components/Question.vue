630c0764fa6114d8ca65b83a7e09f66202cbac29
QuocToanTK95 <quoctoantk95@gmail.com>
2023-06-15 10:13:58 +0700

    QuizApp

---
+++ src/components/Question.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const emit = defineEmits(['selectOption']);
+const { question } = defineProps(['question']);
+const emitSelectedOption = isCorrect => {
+	emit('selectOption', isCorrect);
+};
+</script>
+
+<template>
+	<div class="question-container">
+		<h1 class="question">
+			{{ question.text }}
+		</h1>
+	</div>
+	<div class="answers-container">
+		<div
+			v-for="option in question.options"
+			:key="option.id"
+			class="option"
+			@click="emitSelectedOption(option.isCorrect)"
+		>
+			<p class="option-label">{{ option.label }}</p>
+			<div class="option-value">
+				<p>{{ option.text }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.question-container {
+	margin-top: 20px;
+
+	.question {
+		font-size: 40px;
+		margin-bottom: 20px;
+	}
+}
+
+.answers-container {
+	.option {
+		display: flex;
+		margin-bottom: 20px;
+		cursor: pointer;
+
+		.option-label {
+			background-color: bisque;
+			width: 60px;
+			height: 60px;
+			font-size: 30px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			color: black;
+		}
+
+		.option-value {
+			background-color: rgb(244, 239, 239);
+			width: 100%;
+			font-size: 30px;
+			padding: 0 20px;
+			color: black;
+			display: flex;
+			align-items: center;
+		}
+	}
+}
+</style>
---
+++ src/views/QuizView.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref, computed } from 'vue';
+import Question from '../components/Question.vue';
+import QuizHeader from '../components/QuizHeader.vue';
+import Result from '../components/Result.vue';
+import { useRoute } from 'vue-router';
+import quizzes from '../data/quizzes.json';
+
+const route = useRoute();
+const quizId = parseInt(route.params.id);
+const quiz = quizzes.find(q => q.id == quizId);
+
+const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswer = ref(0);
+const showResults = ref(false);
+
+const questionStatus = computed(() => `${currentQuestionIndex.value}/${quiz.questions.length}`);
+const barPercentage = computed(
+	() => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+
+const onOptionSelected = isCorrect => {
+	if (isCorrect) {
+		numberOfCorrectAnswer.value++;
+	}
+
+	if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+		showResults.value = true;
+	}
+	currentQuestionIndex.value++;
+};
+</script>
+
+<template>
+	<div>
+		<QuizHeader :questionStatus="questionStatus" :barPercentage="barPercentage" />
+		<div>
+			<Question
+				v-if="!showResults"
+				:question="quiz.questions[currentQuestionIndex]"
+				@selectOption="onOptionSelected"
+			/>
+			<Result
+				v-else
+				:quizQuestionLength="quiz.questions.length"
+				:numberOfCorrectAnswer="numberOfCorrectAnswer"
+			/>
+		</div>
+	</div>
+</template>
